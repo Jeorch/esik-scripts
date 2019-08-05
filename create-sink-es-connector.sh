e056c99d40a8753786815b627d276ed286924960
ZhangChi <jeorch@foxmail.com>
2019-08-05 10:42:28 +0800

    fixed postdata string concat error

--- create-sink-es-connector.sh
+++ create-sink-es-connector.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-postdata="{"name": "elasticsearch-sink-${HOSTNAME}","config": {"jobId": "esik_${HOSTNAME//./_}","topics": "esik_${HOSTNAME//./_}","connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector","tasks.max": "1","key.ignore": "true","connection.url": "http://192.168.100.174:9200","type.name": ""}}"
+postdata="{\"name\": \"elasticsearch-sink-${HOSTNAME}\",\"config\": {\"jobId\": \"esik_${HOSTNAME//./_}\",\"topics\": \"esik_${HOSTNAME//./_}\",\"connector.class\": \"io.confluent.connect.elasticsearch.ElasticsearchSinkConnector\",\"tasks.max\": \"1\",\"key.ignore\": \"true\",\"connection.url\": \"http://192.168.100.174:9200\",\"type.name\": \"\"}}"
 echo ${postdata}
 curl -X POST -H "Content-Type: application/json" \
       --data "${postdata}" \
