cdce8dbd34c6ce8915517c24b5afdd9cc23790c4
ZhangChi <jeorch@foxmail.com>
2019-08-08 16:35:04 +0800

    all host send to one topic.

--- start-esik.sh
+++ start-esik.sh
@@ -4,5 +4,6 @@ echo 'Start set env.'
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 export LOGGER_DEBUG=true
 export BM_KAFKA_CONF_HOME=${PWD}/kafkaconfig.json
+export ESIK_TOPIC=esik
 echo 'Start esik.'
 ./esik
